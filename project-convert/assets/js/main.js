02fb70e33d908c605bc13de004bc8af2dbf44b51
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2024-01-02 20:53:48 +0700

    update code b8

--- project-convert/assets/js/main.js
+++ project-convert/assets/js/main.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+
+  $('.owl-carousel').owlCarousel({
+    loop: true, //Vong lap 
+    margin: 10, //K/C giua cac pt
+    nav: true, //Thanh dieu huong
+    dots: true, //Dau cham dieu huong
+    autoplay: true, //Slider tu dong chay
+    autoplayTimeout: 3000, //don vi ms - quy dinh thoi gian chay slider
+    responsive: {
+      0: {
+        items: 1
+      },
+      800: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  })
+
 });
